a0ef3e3745d36e704c065f7a71215f5ab27d9eab
TANUTCHA K <165627864+tanutchak@users.noreply.github.com>
2025-03-12 21:50:36 +0700

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,5 @@
-
+SELECT 
+    Patient_ID,
+    ROUND((JULIANDAY('2025-01-01 ' || Post_Consultation_Time) - 
+           JULIANDAY('2025-01-01 ' || Entry_Time)) * 1440, 2) AS Wait_Time_Minutes
+FROM hospital_data;
